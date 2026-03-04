048a217af60013ee353dfe3d2d6ab2706f28cf24
kishor <kishorsakthinarayanan@gmail.com>
2026-03-04 14:51:05 +0530

    last

--- Backend/server.js
+++ Backend/server.js
@@ -103,5 +103,5 @@ app.delete('/api/cart/:courseId', auth, async (req, res) => {
     res.json({ cart: user.cart });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running at ${PORT}`));
